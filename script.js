b242c04632595bcf6182ab731263834de3f50086
Tom Augspurger <tom.augspurger88@gmail.com>
2016-08-30 07:45:24 -0500

    Return on scroll up

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
-var navbarHeight = $('header').outerHeight();
+var navbarHeight = $('header').outerHeight() + 20;
 
 $(window).scroll(function(event){
     didScroll = true;
@@ -18,22 +18,18 @@ setInterval(function() {
 
 function hasScrolled() {
     var st = $(this).scrollTop();
-
     // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
+    if(st <= delta)
         return;
 
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navbarHeight){
-        console.log('d2u');
         // Scroll Down
         $('header').removeClass('nav-down').addClass('nav-up');
     } else {
         // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
+		$('header').removeClass('nav-up').addClass('nav-down');
     }
 
     lastScrollTop = st;
